8f5f855bf78d2c9ffc4e17f80773508498db0221
HiLow <newbie_wisdom@hotmail.com>
2024-05-27 22:36:59 +0900

    update

--- lib/views/app.dart
+++ lib/views/app.dart
@@ -1,5 +1,6 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:game_box/models/enums/player.dart';
 import 'package:game_box/models/enums/turn.dart';
@@ -62,57 +63,92 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(40.0),
-          child: GestureDetector(
-              onPanStart: (DragStartDetails details) async {
-                if (!_canTap) {
-                  return;
-                }
+          child: Column(
+            children: <Widget>[
+              const Text('オセロ',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 20.0),
+              Flexible(
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: GestureDetector(
+                      onPanStart: (DragStartDetails details) async {
+                        if (!_canTap) {
+                          return;
+                        }
 
-                _canTap = false;
+                        _canTap = false;
 
-                var isSucceeded = false;
+                        var isSucceeded = false;
 
-                setState(() {
-                  final size = getWidgetSize(_key);
-                  if (size != null) {
-                    final cellSize = size.width / _manager.board.size;
-                    final x = (details.localPosition.dx / cellSize).toInt();
-                    final y = (details.localPosition.dy / cellSize).toInt();
+                        setState(() {
+                          final size = getWidgetSize(_key);
+                          if (size != null) {
+                            final cellSize = size.width / _manager.board.size;
+                            final x =
+                                (details.localPosition.dx / cellSize).toInt();
+                            final y =
+                                (details.localPosition.dy / cellSize).toInt();
 
-                    isSucceeded = _manager.next(x, y);
-                  }
-                });
+                            isSucceeded = _manager.next(x, y);
+                          }
+                        });
 
-                if (isSucceeded) {
-                  setState(() => _useHighLight = false);
+                        if (isSucceeded) {
+                          setState(() => _useHighLight = false);
 
-                  if (_audioPlayer.state == PlayerState.playing) {
-                    await _audioPlayer.stop();
-                  }
+                          if (_audioPlayer.state == PlayerState.playing) {
+                            await _audioPlayer.stop();
+                          }
 
-                  await _audioPlayer.play(AssetSource(putSound));
+                          await _audioPlayer.play(AssetSource(putSound));
 
-                  while (!_manager.isFinished &&
-                      isOpponent(_manager.currentTurn)) {
-                    await Future.delayed(const Duration(milliseconds: 500));
+                          while (!_manager.isFinished &&
+                              isOpponent(_manager.currentTurn)) {
+                            await Future.delayed(
+                                const Duration(milliseconds: 500));
 
-                    setState(() => _manager.nextByAI());
-                    await _audioPlayer.play(AssetSource(putSound));
-                  }
+                            setState(() => _manager.nextByAI());
+                            await _audioPlayer.play(AssetSource(putSound));
+                          }
 
-                  setState(() => _useHighLight = true);
-                }
+                          setState(() => _useHighLight = true);
+                        }
 
-                _canTap = true;
-              },
-              child: AspectRatio(
-                aspectRatio: 1,
-                child: CustomPaint(
-                  key: _key,
-                  painter: OthelloPainter(
-                      board: _manager.board, useHighLight: _useHighLight),
+                        _canTap = true;
+                      },
+                      child: CustomPaint(
+                        key: _key,
+                        painter: OthelloPainter(
+                            board: _manager.board, useHighLight: _useHighLight),
+                      )),
                 ),
-              )),
+              ),
+              const SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    width: 200,
+                    height: 50,
+                    child: FilledButton(
+                        style: FilledButton.styleFrom(
+                            backgroundColor: Colors.green[700],
+                            shape: const ContinuousRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(8)))),
+                        onPressed: () {
+                          if (_canTap) {
+                            setState(() => _manager.initialize());
+                          }
+                        },
+                        child:
+                            const Text('リセット', style: TextStyle(fontSize: 24))),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
