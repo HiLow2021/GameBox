1bc2b74e5e0bb2ebc07571a1b46cd72101a1db11
HiLow <newbie_wisdom@hotmail.com>
2024-05-28 22:37:04 +0900

    update

---
+++ lib/models/enums/level.dart
@@ -0,0 +1,1 @@
+enum Level { normal }
--- lib/views/app.dart
+++ lib/views/app.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-
+import 'package:game_box/models/enums/level.dart';
 import 'package:game_box/models/enums/player.dart';
 import 'package:game_box/models/enums/turn.dart';
 import 'package:game_box/models/othello_manager.dart';
@@ -44,6 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
   final _manager = OthelloManager(8);
 
   var _player = Player.black;
+  var _level = Level.normal;
   var _useHighLight = true;
   var _canTap = true;
 
@@ -147,13 +148,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   const SizedBox(height: 20.0),
                   Container(
                     padding: EdgeInsets.symmetric(
-                        horizontal: small ? 30 : 40, vertical: small ? 12 : 20),
+                        horizontal: 20, vertical: small ? 12 : 20),
                     decoration: BoxDecoration(
                         color: Colors.grey[350],
                         border: Border.all(
                             color: const Color.fromARGB(255, 92, 92, 92),
                             width: small ? 2 : 4)),
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: <Widget>[
                         Builder(builder: (context) {
                           final widget = <Widget>[
@@ -207,6 +209,66 @@ class _MyHomePageState extends State<MyHomePage> {
                                       child: Text(value == Player.black
                                           ? '先手(黒)'
                                           : '後手(白)'));
+                                }).toList(),
+                              ),
+                            ),
+                          ];
+
+                          if (small) {
+                            return Column(children: widget);
+                          } else {
+                            return Row(children: widget);
+                          }
+                        }),
+                        Builder(builder: (context) {
+                          final widget = <Widget>[
+                            Text('難易度',
+                                style: TextStyle(
+                                    fontSize: small ? 16 : 20,
+                                    fontWeight: FontWeight.bold)),
+                            SizedBox(
+                                width: small ? 0 : 20, height: small ? 10 : 0),
+                            Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color:
+                                          const Color.fromARGB(255, 92, 92, 92),
+                                      width: 1),
+                                  borderRadius: BorderRadius.circular(6)),
+                              child: DropdownButton<Level>(
+                                value: _level,
+                                focusColor: Colors.transparent,
+                                padding: EdgeInsets.fromLTRB(
+                                    20, 0, 10, small ? 0 : 5),
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: small ? 16 : 20),
+                                underline: const SizedBox(),
+                                onChanged: (Level? value) async {
+                                  if (!_canTap ||
+                                      value == null ||
+                                      _level == value) {
+                                    return;
+                                  }
+
+                                  setState(() {
+                                    _level = value;
+                                    _manager.initialize();
+
+                                    if (_player == Player.white) {
+                                      _manager.nextByAI();
+                                    }
+                                  });
+
+                                  if (_player == Player.white) {
+                                    await _audioPlayer
+                                        .play(AssetSource(putSound));
+                                  }
+                                },
+                                items: Level.values
+                                    .map<DropdownMenuItem<Level>>((value) {
+                                  return DropdownMenuItem(
+                                      value: value, child: const Text('普通'));
                                 }).toList(),
                               ),
                             ),
